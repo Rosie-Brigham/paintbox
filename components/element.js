ee32add027e1cddc42871656754747edd1c21178
Simon Bays <simon.bays@gmail.com>
2019-10-11 14:51:28 +0100

    Update babel to latest version. Add clear button to erase canvas.

--- components/element.js
+++ components/element.js
@@ -1,28 +1,32 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Radium from 'radium'
 
 class Element extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
-    const plain = {width: '15px', height: '15px', fill: '#F9D9FF' }
-    this.state = {hover: false, style: plain};
+    const plain = { width: '15px', height: '15px', fill: '#F9D9FF' }
+    this.state = { hover: false, style: plain }
   }
 
   handleUpdate() {
-    const filled = {width: '15px', height: '15px', fill: this.props.colour }
-    this.setState({hover: true, style: filled});
+    const filled = { width: '15px', height: '15px', fill: this.props.colour }
+    this.setState({ hover: true, style: filled })
   }
 
-  render () {
-    const brushAction = { [this.props.brushAction]: this.handleUpdate.bind(this) }
+  render() {
+    const brushAction = {
+      [this.props.brushAction]: this.handleUpdate.bind(this),
+    }
 
     return (
-      <rect {...brushAction}
-            key={this.props.propKey}
-            style={this.state.style}
-            x={this.props.x}
-            y={this.props.y} />
+      <rect
+        {...brushAction}
+        key={this.props.propKey}
+        style={this.state.style}
+        x={this.props.x}
+        y={this.props.y}
+      />
     )
   }
 }
--- components/elements.js
+++ components/elements.js
@@ -1,41 +1,45 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Element from './element.js'
 
 class Elements extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
+    this.state = {
+      size: parseInt(props.size),
+    }
   }
 
   createElements(size, colour, brushAction) {
-    var array = []
-    for (var x = 0; x < size; x = x + 10) {
-      for (var y = 0; y < size; y = y + 10) {
-        array.push(<Element x={x} y={y} colour={colour} brushAction={brushAction}/>)
+    const array = []
+    for (let x = 0; x < size; x = x + 10) {
+      for (let y = 0; y < size; y = y + 10) {
+        array.push(
+          <Element x={x} y={y} colour={colour} brushAction={brushAction} />
+        )
       }
     }
     return array
   }
 
-  render(){
+  render() {
     let size = parseInt(this.props.size)
-    let array = Array.apply(null, {length: size}).map(Number.call, Number)
+    let array = Array.apply(null, { length: size }).map(Number.call, Number)
 
     let colour = this.props.colour
     let brushAction = this.props.brush
-    let dimension = this.props.size * 10
+    let dimension = this.state.size * 10
 
-    var styles = {
-      width: dimension + 'px',
-      height: dimension + 'px'
+    const styles = {
+      transition: 'width 0.5s, height 0.5s',
+      width: this.state.size * 10 + 'px',
+      height: this.state.size * 10 + 'px',
     }
 
-    let elements= this.createElements(dimension, colour, brushAction)
+    let elements = this.createElements(dimension, colour, brushAction)
 
-    return(
+    return (
       <div>
-        <svg style={styles}>
-          {elements}
-        </svg>
+        <svg style={styles}>{elements}</svg>
       </div>
     )
   }
